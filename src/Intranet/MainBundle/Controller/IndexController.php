83bb34e2014d98d2912a8aa0ec734b0369d21fe8
tsviklinskyi <tsviklinskyi92@gmail.com>
2014-10-21 16:20:35 +0300

    fix usersettings on homepage

--- src/Intranet/MainBundle/Controller/IndexController.php
+++ src/Intranet/MainBundle/Controller/IndexController.php
@@ -3,7 +3,6 @@
 namespace Intranet\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Intranet\MainBundle\Services\Notifier;
 
 class IndexController extends Controller
@@ -11,19 +10,6 @@ class IndexController extends Controller
     public function indexAction()
     {
         return $this->render('IntranetMainBundle:Index:index.html.twig');
-    }
-    
-    public function getUserSettingsAction(){
-    	$user_settings = $this->getUser()->getUserSettings();
-    	$user_settings_notifications = $this->getUser()->getUserSettingsnotifications();
-    	$response = new Response(json_encode(array("result" => 
-    			array(
-    					"user_settings" => $user_settings->getInArray(),
-    					"user_settings_notifications" => $user_settings_notifications->getInArray()
-    			)
-    	)));
-    	$response->headers->set('Content-Type', 'application/json');
-    	return $response;
     }
     
     public function showHomepageAction()
